4eb18a267ad6029ded505d6f200c168787038bfa
DrDimon <sslinneberg@gmail.com>
2016-03-25 15:29:00 +0100

    using other heap

--- botbot/prepare.go
+++ botbot/prepare.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-    "container/heap"
+      "fmt"
+//    "container/heap"
 )
 
 func calculateGoalPriorities() {
-    // Using a floodfill
-    type priorityNode struct {
-        x, y int
-        priority int
-    }
     var priorityMap [70][70]int
     for x := 0; x < 70; x++ {
         for y := 0; y < 70; y++ {
@@ -49,7 +45,7 @@ func calculateGoalPriorities() {
 
 var apspWork int
 
-type PathArray [][]*[][]int16;
+type PathArray [][]*[][]int;
 
 func all_pairs_shortest_path(walls *[70][70]bool, width int, height int) PathArray{
     // TODO Fix...
@@ -57,47 +53,21 @@ func all_pairs_shortest_path(walls *[70][70]bool, width int, height int) PathArr
     height -= 1;
 
     var pairs PathArray;
-    pairs = make([][]*[][]int16, width);
+    pairs = make([][]*[][]int, width);
 
     for x := 0; x < width; x++ {
-        pairs[x] = make([]*[][]int16, height);
+        pairs[x] = make([]*[][]int, height);
         for y := 0; y < height; y++ {
             pairs[x][y] = shortest_path(x, y, walls, width, height);
         }
     }
 
-    // PRINT All Pairs shortest path
-    //for y := 0; y < height; y++ {
-    //  for x := 0; x < width; x++ {
-    //      fmt.Print(x);
-    //      fmt.Print(";");
-    //      print(y);
-    //      if(pairs[x][y] != nil){
-    //        for i := 0; i < height; i++ {
-    //          for j := 0; j < width; j++ {
-    //              printf("%02d ", (*pairs[x][y])[j][i]);
-    //          }
-    //          print("");
-    //        }
-    //      } else {
-    //        fmt.Print("N ");
-    //      }
-    //      print("");
-    //  }
-    //  print("");
-    //}
-
-
-    //TEST TODO: Remove
-    print(distance(pairs, 1, 1, 2, 1));
-    print(distance(pairs, 1, 1, 0, 1));
-    print(distance(pairs, 3, 1, 3, 3));
-    getShortestPath(pairs, 3, 1, 3, 3, width, height);
+    printAPSP(width, height, pairs);
 
     return pairs;
 }
 
-func distance(apsp PathArray, xStart int, yStart int, xEnd int, yEnd int) int16{
+func distance(apsp PathArray, xStart int, yStart int, xEnd int, yEnd int) int{
     if(apsp[xStart][yStart] == nil || apsp[xEnd][yEnd] == nil){
         return -1;
     }
@@ -105,7 +75,7 @@ func distance(apsp PathArray, xStart int, yStart int, xEnd int, yEnd int) int16{
     return (*apsp[xStart][yStart])[xEnd][yEnd];
 }
 
-func checked_distance(apsp PathArray, xStart int, yStart int, xEnd int, yEnd int, width int, height int) int16 {
+func checked_distance(apsp PathArray, xStart int, yStart int, xEnd int, yEnd int, width int, height int) int {
     if(isInside(xStart, yStart, width, height) && isInside(xEnd, yEnd, width, height)){
         return distance(apsp, xStart, yStart, xEnd, yEnd);
     }
@@ -162,10 +132,15 @@ func getShortestPath(apsp PathArray, xStart int, yStart int, xEnd int, yEnd int,
 }
 
 // HELPER FUNCTIONS:
-func shortest_path(x int, y int, walls *[70][70]bool, width int, height int) *[][]int16 {
+type priorityNode struct {
+    x, y int
+    priority int
+}
 
-    var arr [][]int16;
-    arr = make([][]int16, width);
+func shortest_path(x int, y int, walls *[70][70]bool, width int, height int) *[][]int {
+
+    var arr [][]int;
+    arr = make([][]int, width);
     var added [70][70]bool;
 
     if(walls[x][y]){
@@ -173,7 +148,7 @@ func shortest_path(x int, y int, walls *[70][70]bool, width int, height int) *[]
     }
 
     for i := 0; i < width; i++ {
-        arr[i] = make([]int16, height);
+        arr[i] = make([]int, height);
         for j := 0; j < height; j++ {
             apspWork++
             arr[i][j] = -1;
@@ -181,36 +156,28 @@ func shortest_path(x int, y int, walls *[70][70]bool, width int, height int) *[]
         }
     }
 
-    first :=  Cell{
-        x: x,
-        y: y,
-        length: 0,
-        index: 0,
-    }
-    cells := &PriorityQueue{&first};
-    heap.Init(cells);
-    added[x][y] = true;
-    for cells.Len() > 0 {
-        currentCell := *heap.Pop(cells).(*Cell);
-        arr[currentCell.x][currentCell.y] = currentCell.length;
-        add(currentCell.x+1, currentCell.y, currentCell.length, &added, walls, width, height, cells);
-        add(currentCell.x-1, currentCell.y, currentCell.length, &added, walls, width, height, cells);
-        add(currentCell.x, currentCell.y+1, currentCell.length, &added, walls, width, height, cells);
-        add(currentCell.x, currentCell.y-1, currentCell.length, &added, walls, width, height, cells);
+    first :=  priorityNode{x, y, 0}
+
+    var cells Heap
+    cells.Insert(first, 0)
+
+    added[x][y] = true
+    for !cells.IsEmpty() {
+        currentCell := cells.Extract().(priorityNode)
+        arr[currentCell.x][currentCell.y] = currentCell.priority
+        add(currentCell.x+1, currentCell.y, currentCell.priority, &added, walls, width, height, &cells)
+        add(currentCell.x-1, currentCell.y, currentCell.priority, &added, walls, width, height, &cells)
+        add(currentCell.x, currentCell.y+1, currentCell.priority, &added, walls, width, height, &cells)
+        add(currentCell.x, currentCell.y-1, currentCell.priority, &added, walls, width, height, &cells)
     }
     return &arr;
 }
 
-func add(x int, y int, length int16, added *[70][70]bool, walls *[70][70]bool, width int, height int, cells *PriorityQueue){
+func add(x int, y int, length int, added *[70][70]bool, walls *[70][70]bool, width int, height int, cells* Heap){
     if( isInside(x, y, width, height) && !walls[x][y] && !added[x][y]){
-        newCell := Cell{
-            x: x,
-            y: y,
-            length: length+1,
-            index: 0,
-        }
-        added[x][y] = true;
-        heap.Push(cells, &newCell);
+        newCell := priorityNode{x, y, length+1}
+        added[x][y] = true
+        (*cells).Insert(newCell, length+1)
     }
 }
 
@@ -218,47 +185,25 @@ func isInside(x int, y int, width int, height int) bool {
     return x >= 0 && x <= width-1 && y >= 0 && y <= height-1 ;
 }
 
-
-// Used for the heap in shortest_path
-// Inspired by golang.org example
-type Cell struct {
-    x int;
-    y int;
-    length int16;
-    index int;
-}
-
-type PriorityQueue []*Cell
-
-func (pq PriorityQueue) Len() int { return len(pq) }
-
-func (pq PriorityQueue) Less(i, j int) bool {
-    return (*pq[i]).length < (*pq[j]).length
-}
-
-func (pq PriorityQueue) Swap(i, j int) {
-    pq[i], pq[j] = pq[j], pq[i]
-    (*pq[i]).index = i
-    (*pq[j]).index = j
-}
-
-func (pq *PriorityQueue) Push(x interface{}) {
-    n := len(*pq)
-    item := x.(*Cell)
-    item.index = n
-    *pq = append(*pq, item)
-}
-
-func (pq *PriorityQueue) Pop() interface{} {
-    old := *pq
-    n := len(old)
-    item := old[n-1]
-    (*item).index = -1;
-    *pq = old[0 : n-1]
-    return item
-}
-
-func (pq *PriorityQueue) update(item *Cell, length int16) {
-    (*item).length = length
-    heap.Fix(pq, item.index)
+// PRINT All Pairs shortest path
+func printAPSP(width int, height int, pairs PathArray){
+    for y := 0; y < height; y++ {
+      for x := 0; x < width; x++ {
+          fmt.Print(x);
+          fmt.Print(";");
+          print(y);
+          if(pairs[x][y] != nil){
+            for i := 0; i < height; i++ {
+              for j := 0; j < width; j++ {
+                  printf("%02d ", (*pairs[x][y])[j][i]);
+              }
+              print("");
+            }
+          } else {
+            fmt.Print("N ");
+          }
+          print("");
+      }
+      print("");
+    }
 }
